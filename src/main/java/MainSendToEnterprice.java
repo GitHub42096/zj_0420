0bf667801f628c530c8ea069374564f80bf696af
zhangjie <jie.zhang@yingxiong.com>
2018-04-20 18:34:59 +0800

    send email to enterprice email

---
+++ src/main/java/MainSendToEnterprice.java
@@ -0,0 +1,74 @@
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Date;
+import java.util.Properties;
+
+public class MainSendToEnterprice {
+
+    public static String myEmailAccount = "jie.zhang@yingxiong.com";
+    public static String myEmailPassword = "Zj420964597";
+    public static String myEmailSMTPHost = "smtp.exmail.qq.com";
+    public static String receiveMailAccount = "junhong.lin@yingxiong.com";
+
+    public static void main(String[] args) throws Exception {
+        final String smtpPort = "465";
+        Properties props = new Properties();
+        props.setProperty("mail.transport.protocol", "smtp");
+        props.setProperty("mail.smtp.host", myEmailSMTPHost);
+        props.setProperty("mail.smtp.port", smtpPort);
+        props.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.setProperty("mail.smtp.socketFactory.fallback", "false");
+        props.setProperty("mail.smtp.socketFactory.port", smtpPort);
+
+        Session session = Session.getInstance(props);
+        session.setDebug(true);
+        // 3. 创建一封邮件
+        MimeMessage message = createMimeMessage(session, myEmailAccount, receiveMailAccount);
+        // 4. 根据 Session 获取邮件传输对象
+        Transport transport = session.getTransport();
+        // 5. 使用 邮箱账号 和 密码 连接邮件服务器
+        transport.connect(myEmailAccount, myEmailPassword);
+        // 6. 发送邮件
+        transport.sendMessage(message, message.getAllRecipients());
+        // 7. 关闭连接
+        transport.close();
+        System.out.println("成功");
+    }
+
+    /**
+     * 创建一封只包含文本的简单邮件
+     *
+     * @param session 和服务器交互的会话
+     * @param sendMail 发件人邮箱
+     * @param receiveMail 收件人邮箱
+     * @return
+     * @throws Exception
+     */
+    public static MimeMessage createMimeMessage(Session session, String sendMail, String receiveMail) throws Exception {
+        // 1. 创建一封邮件
+        MimeMessage message = new MimeMessage(session);
+
+        // 2. From: 发件人（昵称有广告嫌疑，避免被邮件服务器误认为是滥发广告以至返回失败，请修改昵称）
+        message.setFrom(new InternetAddress(sendMail, "GUESS WHO I AM", "UTF-8"));
+
+        // 3. To: 收件人（可以增加多个收件人、抄送、密送）
+        message.setRecipient(MimeMessage.RecipientType.TO, new InternetAddress(receiveMail, "SB", "UTF-8"));
+
+        // 4. Subject: 邮件主题（标题有广告嫌疑，避免被邮件服务器误认为是滥发广告以至返回失败，请修改标题）
+        message.setSubject("YOU ARE PIG", "UTF-8");
+
+        // 5. Content: 邮件正文（可以使用html标签）（内容有广告嫌疑，避免被邮件服务器误认为是滥发广告以至返回失败，请修改发送内容）
+        message.setContent("I JUST WANT TO TEST MY PROJECT", "text/html;charset=UTF-8");
+
+        // 6. 设置发件时间
+        message.setSentDate(new Date());
+
+        // 7. 保存设置
+        message.saveChanges();
+
+        return message;
+    }
+
+}
